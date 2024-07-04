d17ab849b30b77de177fca98dde5b9db4204702c
Aleksandr Kovalev <ReiMowakari@yandex.ru>
2024-07-04 11:20:57 +0300

    'third review'

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -6,33 +6,45 @@ from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 from posts.models import Post, Group
 
 
-class AuthorizedModelMixin(ModelViewSet):
+class AuthorizedModelMixin:
     """
     Базовый миксин для авторизованного автора для изменения собственных данных.
-    Вынес в отдельный миксин, основанный на ModelViewSet для того,
+    Вынес в отдельный миксин, для того,
     чтобы исключить повторения кода в вьюсетах постов и комментариев.
     """
 
-    # Проверяем изменение данных только автором.
     def perform_update(self, serializer):
+        """
+        Проверяем изменение данных только автором.
+        :param serializer:
+        :return: обновление объекта.
+        """
         if serializer.instance.author != self.request.user:
             raise PermissionDenied('Изменение чужих данных запрещено!')
         super().perform_update(serializer)
 
-    # Проверяем удаление данных только автором.
     def perform_destroy(self, instance):
+        """
+        Проверяем удаление данных только автором.
+        :param instance:
+        :return: удаление объекта.
+        """
         if instance.author != self.request.user:
             raise PermissionDenied('Удаление чужих данных запрещено!')
         super().perform_destroy(instance)
 
 
-class PostViewSet(AuthorizedModelMixin):
+class PostViewSet(AuthorizedModelMixin, ModelViewSet):
     """Вьюсет для обработки постов."""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    # Переопределяем метод для автоматического добавления автора поста.
     def perform_create(self, serializer):
+        """
+        Переопределяем метод для автоматического добавления автора поста.
+        :param serializer:
+        :return: создаем объект поста.
+        """
         serializer.save(author=self.request.user)
 
 
@@ -42,21 +54,30 @@ class GroupViewSet(ReadOnlyModelViewSet):
     serializer_class = GroupSerializer
 
 
-class CommentViewSet(AuthorizedModelMixin):
+class CommentViewSet(AuthorizedModelMixin, ModelViewSet):
     serializer_class = CommentSerializer
 
-    # Добавляем метод для получения отдельного поста.
     def get_post(self):
+        """
+        Метод для получения отдельного поста.
+        :return:
+        """
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         return post
 
-    # Переопределяем получение объекта поста с комментариями.
     def get_queryset(self):
-        """Проверяем наличие поста и возвращаем объект с комментариями."""
+        """
+        Проверяем наличие поста.
+        :return: возвращаем объект с комментариями
+        """
         return self.get_post().comments.all()
 
-    # Переопределяем метод для автоматического добавления автора поста.
     def perform_create(self, serializer):
+        """
+        Переопределяем метод для автоматического добавления автора поста.
+        :param serializer:
+        :return: объект поста с автором.
+        """
         serializer.save(
             author=self.request.user,
             post=self.get_post())
